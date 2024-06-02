806af15b0ae381cbe228eff8c1e33f1bc0645f43
UdayDiverse <uday.m@diverseinfotech.com>
2024-06-02 23:28:34 +0530

    Add project files.

---
+++ CleaningWebsite/Controllers/DrivingSchoolController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrivingSchoolWebsite.Controllers
+{
+    public class DrivingSchoolController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
